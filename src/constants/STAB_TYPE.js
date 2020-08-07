994164c2f4f215ce5c7ce6d8e48e860d21a82d51
Sendy Aditya Suryana <sendy.suryana@go-jek.com>
2020-08-07 12:42:15 +0700

    Add constant

---
+++ src/constants/STAB_TYPE.js
@@ -0,0 +1,6 @@
+export const STAB_TYPE = {
+  CHERRY_COSTAR: { text: "Cherry + Costar Stabilizer", value: 1 },
+  CHERRY_ONLY: { text: "Cherry Stabilizer", value: 2 },
+  COSTAR_ONLY: { text: "Costar Stabilizer", value: 3 },
+  ALPS_ONLY: { text: "Alps Stabilizer", value: 4 }
+};
